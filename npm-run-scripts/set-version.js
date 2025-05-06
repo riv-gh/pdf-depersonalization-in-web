4114cabb3189cd81e9097c6428e226611edc25fa
daenur <daenur@outlook.com>
2025-05-06 10:42:45 +0300

    fix path
    On branch main
    Changes to be committed:
    	modified:   npm-run-scripts/set-version.js

--- npm-run-scripts/set-version.js
+++ npm-run-scripts/set-version.js
@@ -2,9 +2,9 @@ console.log('Setting version...');
 const pkg=require('./../package.json');
 pkg.version=process.argv[2];
 if (!pkg.version) {
-  console.error('Version not specified. Usage: node package-set-version.js <version>');
+  console.error('Version not specified. Usage: npm run set-version <version>');
   process.exit(1);
 }
-require('fs').writeFileSync('./../package.json', JSON.stringify(pkg, null, 2));
-require('child_process').execSync(`git add package.json && git commit -m "Set version: ${pkg.version}" && git tag ${pkg.version}`, {stdio: 'inherit', cwd: '..'});
+require('fs').writeFileSync('package.json', JSON.stringify(pkg, null, 2));
+require('child_process').execSync(`git add package.json && git commit -m "Set version: ${pkg.version}" && git tag ${pkg.version}`, {stdio: 'inherit', cwd: '.'});
 console.log(`Set version: ${pkg.version}`);
