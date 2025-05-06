81a218d75b565903ff8298a61548c17ff4fd8b33
daenur <daenur@outlook.com>
2025-05-06 09:32:23 +0300

    fix filenames Changes to be committed: 	renamed:    npm-run-scripts/set-vesion.js -> npm-run-scripts/set-version.js

--- npm-run-scripts/set-version.js
+++ npm-run-scripts/set-version.js
@@ -1,10 +1,10 @@
 console.log('Setting version...');
-const pkg=require('./package.json');
+const pkg=require('./../package.json');
 pkg.version=process.argv[2];
 if (!pkg.version) {
   console.error('Version not specified. Usage: node package-set-version.js <version>');
   process.exit(1);
 }
-require('fs').writeFileSync('./package.json', JSON.stringify(pkg, null, 2));
+require('fs').writeFileSync('./../package.json', JSON.stringify(pkg, null, 2));
 require('child_process').execSync(`git add package.json && git commit -m "Set version: ${pkg.version}" && git tag ${pkg.version}`, {stdio: 'inherit'});
 console.log(`Set version: ${pkg.version}`);
